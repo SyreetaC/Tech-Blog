304c4bf2f2cdcd16737082b89c2c61df0f1022f5
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-13 08:22:05 +0100

    get all posts on render homepage

--- src/controllers/html/renderHomePage.js
+++ src/controllers/html/renderHomePage.js
@@ -1,6 +1,22 @@
 const renderHomePage = (req, res) => {
   //get all posts
-  //send posts to handlebars
+  Post.findAll({
+    attributes: ["id", "title", "body", "user_id"],
+    include: [
+      {
+        model: User,
+        as: "user",
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        as: "comments",
+        attributes: ["id", "message", "user_id"],
+      },
+    ],
+  });
+
+  //TO DO send posts to handlebars
   res.render("homepage");
 };
 
