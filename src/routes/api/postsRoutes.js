d743a6a2565976ffda15d6b98b989cd038ee1894
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-14 21:42:44 +0100

    Create post wip

--- src/routes/api/postsRoutes.js
+++ src/routes/api/postsRoutes.js
@@ -1,13 +1,13 @@
-// const { Router } = require("express");
+const { Router } = require("express");
 
 // const handleCommentForPost = require("../../controllers/api/handleCommentForPost");
-// const handleCreatePost = require("../../controllers/api/handleCreatePost");
+const handleCreatePost = require("../../controllers/api/handleCreatePost");
 // const handleDeletePost = require("../../controllers/api/handleDeletePost");
 
-// const router = Router();
+const router = Router();
 
 // router.post("/:id/comments", handleCommentForPost);
 // router.delete("/:id", handleDeletePost);
-// router.post("/new-post", handleCreatePost);
+router.post("/new-post", handleCreatePost);
 
-// module.exports = router;
+module.exports = router;
