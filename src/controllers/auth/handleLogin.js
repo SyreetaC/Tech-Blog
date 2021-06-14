69546a0013fd1e2c30773e5d423a8dd32e2ab03d
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-14 21:49:10 +0100

    changed error codes

--- src/controllers/auth/handleLogin.js
+++ src/controllers/auth/handleLogin.js
@@ -1,22 +1,22 @@
 const { User } = require("../../models");
 
 const handleLogin = async (req, res) => {
-  console.log("Handle login");
   try {
     const { username, password } = req.body;
 
     const user = await User.findOne({ where: { username } });
+    console.log(user);
 
     if (!user) {
       console.log("User does not exist");
-      return res.status(404).json({ error: "Failed to login" });
+      return res.status(401).json({ error: "Failed to login" });
     }
 
     const validPassword = await user.isPasswordValid(password);
 
     if (!validPassword) {
       console.log("Invalid password");
-      return res.status(404).json({ error: "Failed to login" });
+      return res.status(401).json({ error: "Failed to login" });
     }
 
     req.session.save(() => {
