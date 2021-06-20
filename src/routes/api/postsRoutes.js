d6d52aad0e9d0e13d06291f88cd66b73199f5a52
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-20 19:51:16 +0100

    add view and delete buttons

--- src/routes/api/postsRoutes.js
+++ src/routes/api/postsRoutes.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 
-// const handleCommentForPost = require("../../controllers/api/handleCommentForPost");
+const handleCommentForPost = require("../../controllers/api/handleCommentForPost");
 const handleCreatePost = require("../../controllers/api/handleCreatePost");
-// const handleDeletePost = require("../../controllers/api/handleDeletePost");
+const handleDeletePost = require("../../controllers/api/handleDeletePost");
 
 const router = Router();
 
-// router.post("/:id/comments", handleCommentForPost);
-// router.delete("/:id", handleDeletePost);
+router.post("/:id/comments", handleCommentForPost);
+router.delete("/:id", handleDeletePost);
 router.post("/", handleCreatePost);
 
 module.exports = router;
